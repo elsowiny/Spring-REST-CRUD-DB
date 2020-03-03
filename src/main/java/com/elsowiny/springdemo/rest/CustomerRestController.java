33d0b3a5813d349212e4223df3da3f547f037d3a
Sherief Elsowiny <elsowiny@yahoo.com>
2020-03-03 10:24:50 -0500

    added POSTmapping

--- src/main/java/com/elsowiny/springdemo/rest/CustomerRestController.java
+++ src/main/java/com/elsowiny/springdemo/rest/CustomerRestController.java
@@ -5,6 +5,8 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -39,4 +41,15 @@ public class CustomerRestController {
 		return theCustomer;
 	}
 	
+	// add mapping for POSt /customers - add new customer
+	@PostMapping("/customers")
+	public Customer addCustomer(@RequestBody Customer theCustomer) {
+		//incase they pass an id in JSON, we will set it to 0
+		//will force a save instead of an update
+		theCustomer.setId(0);
+		//if ID = 0, DAO will insert new customer
+		customerService.saveCustomer(theCustomer);
+		
+		return theCustomer;
+	}
 }
